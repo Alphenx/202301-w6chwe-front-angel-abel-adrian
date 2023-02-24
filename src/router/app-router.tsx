dcbc8bb3cc43a636e87c7aff0263a9f949d0a285
Adrian_G <adriangarcia.comunicacion@gmail.com>
2023-02-24 12:37:04 +0100

    Add app-router

---
+++ src/router/app-router.tsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from 'react-router-dom';
+import MainLayout from '../layout/MainLayout/MainLayout';
+import CreateRobot from '../pages/CreateRobot/CreateRobot';
+import Home from '../pages/Home/Home';
+import NotFound from '../pages/NotFound/NotFound';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainLayout />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        path: '',
+        element: <Home />,
+      },
+      {
+        path: 'create',
+        element: <CreateRobot />,
+      },
+    ],
+  },
+]);
