e87920d4159fab819c63fb1d4e09bd94cc7ed5cd
Adrian_G <adriangarcia.comunicacion@gmail.com>
2023-02-26 15:37:53 +0100

    Add Form component structure

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import Form from '../../features/Form/Form';
 import './Home.css';
 const Home = () => {
-  return <section className="home">Home</section>;
+  return (
+    <section className="home">
+      Home
+      <Form />
+    </section>
+  );
 };
 
 export default Home;
